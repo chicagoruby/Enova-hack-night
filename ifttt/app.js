af61b0c30b1e7065efaf8a91030fe063a08295c1
Brian Shirai <brixen@gmail.com>
2015-01-14 19:20:38 -0600

    Added files from ifttt and reddit_to_twitter.

---
+++ ifttt/app.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var webhook = require('express-ifttt-webhook');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+
+app.use(webhook(function(json, done) {
+  // transform data
+  var out = json;
+
+  // specify URL to forward your transformed data to
+    out.url = 'INSERT YOUR SINATRA APP URL HERE';
+  // for example: 'http://pacific-eyrie-9417.herokuapp.com'
+
+  done(null, out);
+}));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+app.get('/', function(req, res) {
+  return res.redirect('http://pacific-eyrie-9417.herokuapp.com');
+});
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
